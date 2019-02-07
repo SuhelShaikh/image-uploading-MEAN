a2ddc9fe8f1b232cfa2d36d4de314559ceb9739f
Suhel Shabir Shaikh <suhailshaikh14@gmail.com>
2019-02-07 07:07:25 +0530

    Add files via upload

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+
+import { PostsService } from "../posts.service";
+import { Post } from "../post.model";
+import { mimeType } from "./mime-type.validator";
+import { Subscription } from "rxjs";
+
+@Component({
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
+})
+export class PostCreateComponent implements OnInit {
+  enteredTitle = "";
+  enteredContent = "";
+  post: Post;
+  form: FormGroup;
+  imagePreview: string;
+  private mode = "create";
+  private postId: string;
+  posts: Post[] = [];
+  private postsSub: Subscription;
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
+    });
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+        this.mode = "create";
+        this.postId = null;
+    });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get("image").updateValueAndValidity();
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
+      return;
+    }
+    this.postsService.addPost(
+      this.form.value.title,
+      this.form.value.content,
+      this.form.value.image
+    );
+      alert("Image Uploaded successfully");
+    //this.form.reset();
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+}
